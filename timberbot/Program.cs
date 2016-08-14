9e65670b675f8b6cd836f1b28ff28713d11a486d
Hyeon Kim <simnalamburt@gmail.com>
2016-08-14 22:18:58 +0900

    화면 전체 캡처하는 프로그램

--- timberbot/Program.cs
+++ timberbot/Program.cs
@@ -3,6 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Windows.Forms;
 
 namespace timberbot
 {
@@ -10,6 +13,17 @@ namespace timberbot
     {
         static void Main(string[] args)
         {
+            Rectangle bounds = Screen.GetBounds(Point.Empty);
+            using (Bitmap bitmap = new Bitmap(bounds.Width, bounds.Height))
+            {
+                using (Graphics g = Graphics.FromImage(bitmap))
+                {
+                    g.CopyFromScreen(Point.Empty, Point.Empty, bounds.Size);
+                }
+                bitmap.Save("test.jpg", ImageFormat.Jpeg);
+            }
+
+            Console.Out.WriteLine("Hello, world!");
         }
     }
 }
