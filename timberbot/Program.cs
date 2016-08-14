12c52a9c3b97645b5e9bccfba5b32f25ee43b643
Hyeon Kim <simnalamburt@gmail.com>
2016-08-14 22:45:39 +0900

    화면 캡처 성공

--- timberbot/Program.cs
+++ timberbot/Program.cs
@@ -14,17 +14,59 @@ namespace timberbot
     {
         static int Main(string[] args)
         {
+            // Retrieve windows handle
             var handle = FindWindow("UnityWndClass", "Timberman");
-            if ((long)handle == 0) {
+            if ((long)handle == 0)
+            {
                 Console.Out.WriteLine("Please run Timberman first.");
                 return 1;
             }
 
-            Console.Out.WriteLine(handle);
+            // Get window rect from handle
+            RECT rect;
+            if (!GetWindowRect(handle, out rect))
+            {
+                MessageBox.Show("We somehow failed at `GetWindowRect`");
+                return 1;
+            }
+
+            // RECT -> Rectangle
+            Rectangle bounds = new Rectangle();
+            bounds.X = rect.Left;
+            bounds.Y = rect.Top;
+            bounds.Width = rect.Right - rect.Left + 1;
+            bounds.Height = rect.Bottom - rect.Top + 1;
+
+            // Capture
+            using (Bitmap bitmap = new Bitmap(bounds.Width, bounds.Height))
+            {
+                using (Graphics g = Graphics.FromImage(bitmap))
+                {
+                    g.CopyFromScreen(new Point(bounds.Left,bounds.Top), Point.Empty, bounds.Size);
+                }
+                bitmap.Save("capture.jpg", ImageFormat.Jpeg);
+            }
             return 0;
         }
 
+        //
+        // PInvokes
+        //
+
         [DllImport("user32.dll", SetLastError = true)]
         static extern IntPtr FindWindow(string lpClassName, string lpWindowName);
+
+        [DllImport("user32.dll")]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        static extern bool GetWindowRect(IntPtr hWnd, out RECT lpRect);
+
+        [StructLayout(LayoutKind.Sequential)]
+        public struct RECT
+        {
+            public int Left;        // x position of upper-left corner
+            public int Top;         // y position of upper-left corner
+            public int Right;       // x position of lower-right corner
+            public int Bottom;      // y position of lower-right corner
+        }
     }
 }
